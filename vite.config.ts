07ef8e488d35e32a2eb8ee4e6f2a3fc8b838eb91
xiexiansheng <13570581065@163.com>
2026-05-20 15:44:06 +0800

    feat: bundle as single HTML file via vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: './',
+  build: {
+    assetsInlineLimit: 100_000_000,
+    chunkSizeWarningLimit: 100_000,
+  },
 })
